fixed scaling of force load

xlb/experimental/thermo_mechanical/multigrid.py
@@ -10,34 +10,36 @@ import xlb.experimental.thermo_mechanical.solid_utils as utils
 import math
 from typing import Any
 
-class Level:
 
+class Level:
     def __init__(self, nodes_x, nodes_y, dx, dt, force_load, gamma, compute_backend, velocity_set, precision_policy):
         self.grid = grid_factory((nodes_x, nodes_y), compute_backend=compute_backend)
         self.nodes_x = nodes_x
         self.nodes_y = nodes_y
         self.velocity_set = velocity_set
-        #params needed to set up simulation params
+        # params needed to set up simulation params
         self.gamma = gamma
         self.dx = dx
         self.dt = dt
         self.startup()
-        #setup grids
+        # setup grids
         self.f_1 = self.grid.create_field(cardinality=velocity_set.q, dtype=precision_policy.store_precision)
         self.f_2 = self.grid.create_field(cardinality=velocity_set.q, dtype=precision_policy.store_precision)
         self.f_3 = self.grid.create_field(cardinality=velocity_set.q, dtype=precision_policy.store_precision)
         self.f_4 = self.grid.create_field(cardinality=velocity_set.q, dtype=precision_policy.store_precision)
         self.residual = self.grid.create_field(cardinality=velocity_set.q, dtype=precision_policy.store_precision)
         self.defect_correction = self.grid.create_field(cardinality=velocity_set.q, dtype=precision_policy.store_precision)
-        #setup stepper
+        # setup stepper
         self.stepper = SolidsStepper(self.grid, force_load)
 
         @wp.kernel
-        def relaxation(f_after_stream: Any, f_previous: Any, defect_correction: Any,f_destination: Any,  gamma: wp.float32):
-            i,j,k = wp.tid()
+        def relaxation(f_after_stream: Any, f_previous: Any, defect_correction: Any, f_destination: Any, gamma: wp.float32):
+            i, j, k = wp.tid()
             for l in range(velocity_set.q):
-                f_destination[l,i,j,0] = gamma*(f_after_stream[l,i,j,0] + defect_correction[l,i,j,0]) + (1. - gamma)*f_previous[l,i,j,0] 
-        
+                f_destination[l, i, j, 0] = (
+                    gamma * (f_after_stream[l, i, j, 0] + defect_correction[l, i, j, 0]) + (1.0 - gamma) * f_previous[l, i, j, 0]
+                )
+
         self.relax = relaxation
 
     def startup(self):
@@ -48,10 +50,8 @@ class Level:
         theta = solid_simulation.theta
         solid_simulation.set_parameters(E=E, nu=nu, dx=self.dx, dt=self.dt, L=self.dx, T=self.dt, kappa=kappa, theta=theta)
 
-
-
     def set_defect_correction(self):
-        self.stepper(self.f_4, self.f_3) #perform one step of operator on restricted finer grid approximation
+        self.stepper(self.f_4, self.f_3)  # perform one step of operator on restricted finer grid approximation
         self.f_4, self.f_3 = self.f_3, self.f_4
         wp.launch(utils.add_populations, inputs=[self.f_4, self.residual, self.defect_correction, 9], dim=self.f_4.shape[1:])
 
@@ -59,19 +59,64 @@ class Level:
         wp.launch(utils.subtract_populations, inputs=[self.f_1, self.f_4, self.f_3, 9], dim=self.f_1.shape[1:])
         return self.f_3
 
-    def perform_smoothing(self, get_residual = False):
+    def perform_smoothing(self, get_residual=False):
+        self.startup()
         wp.launch(utils.copy_populations, inputs=[self.f_1, self.f_3, 9], dim=self.f_1.shape[1:])
         self.stepper(self.f_1, self.f_2)
-        #self.f_1, self.f_2 = self.f_2, self.f_1
-        wp.launch(utils.add_populations, inputs=[self.f_2, self.defect_correction, self.f_2, 9], dim=self.f_1.shape[1:])
-        wp.launch(self.relax, inputs=[self.f_2, self.f_3, self.defect_correction, self.f_1, self.gamma], dim=self.f_1.shape[1:])
+        self.f_1, self.f_2 = self.f_2, self.f_1
+        # wp.launch(utils.add_populations, inputs=[self.f_2, self.defect_correction, self.f_2, 9], dim=self.f_1.shape[1:])
+        # wp.launch(self.relax, inputs=[self.f_2, self.f_3, self.defect_correction, self.f_1, self.gamma], dim=self.f_1.shape[1:])
 
         if get_residual:
             wp.launch(utils.get_residual, inputs=[self.f_3, self.f_1, self.residual, self.velocity_set.q], dim=self.f_1.shape[1:])
 
-
     def get_macroscopics(self):
         return self.stepper.get_macroscopics(self.f_1)
+    
+    def info(self):
+        """
+        Print detailed information about this Level.
+        """
+        print("\n===== Level Information =====")
+        print(f"Grid Size: {self.nodes_x} x {self.nodes_y} nodes")
+        print(f"Physical Parameters:")
+        print(f"  - dx: {self.dx}")
+        print(f"  - dt: {self.dt}")
+        print(f"  - gamma (relaxation parameter): {self.gamma}")
+        
+        # Get velocity set information
+        print(f"Velocity Set: {self.velocity_set.__class__.__name__}")
+        print(f"  - q (number of velocities): {self.velocity_set.q}")
+        
+        # Field information
+        print("\nFields:")
+        print(f"  - f_1 shape: {self.f_1.shape}")
+        print(f"  - f_2 shape: {self.f_2.shape}")
+        print(f"  - f_3 shape: {self.f_3.shape}")
+        print(f"  - f_4 shape: {self.f_4.shape}")
+        print(f"  - residual shape: {self.residual.shape}")
+        print(f"  - defect_correction shape: {self.defect_correction.shape}")
+        
+        # Stepper information
+        print("\nStepper Information:")
+        print(f"  - Type: {self.stepper.__class__.__name__}")
+    
+        # Try to get parameters from the SimulationParams
+        try:
+            solid_simulation = SimulationParams()
+            E = solid_simulation.E_unscaled
+            nu = solid_simulation.nu_unscaled
+            kappa = solid_simulation.kappa
+            theta = solid_simulation.theta
+            print("\nSimulation Parameters:")
+            print(f"  - E (Young's modulus): {E}")
+            print(f"  - nu (Poisson ratio): {nu}")
+            print(f"  - kappa: {kappa}")
+            print(f"  - theta: {theta}")
+        except Exception as e:
+            print(f"\nCould not retrieve simulation parameters: {e}")
+        
+        print("============================\n")
 
 
 @wp.kernel
@@ -82,24 +127,29 @@ def interpolate(coarse: Any, fine: Any, nodes_x_coarse: wp.int32, nodes_y_coarse
     nodes_y_fine = nodes_y_coarse * 2
 
     for l in range(dim):
-        fine[l, 2*i, 2*j, 0] = coarse[l, i, j, 0]
-        fine[l, wp.mod(2*i+1, nodes_x_fine), 2*j, 0] = 0.5*(coarse[l, i, j, 0] + coarse[l, wp.mod(i+1, nodes_x_coarse), j, 0])
-        fine[l, i, wp.mod(2*j+1, nodes_y_fine), 0] = 0.5*(coarse[l, i, j, 0] + coarse[l, i, wp.mod(j+1, nodes_y_coarse), 0])
-        fine[l, wp.mod(2*i+1, nodes_x_fine), wp.mod(2*j+1, nodes_y_fine), 0] = 0.25*(coarse[l,i,j,0] + coarse[l,wp.mod(i+1, nodes_x_coarse), j, 0] + coarse[l,i,wp.mod(j+1,nodes_y_coarse), 0] + coarse[l, wp.mod(i+1, nodes_x_coarse), wp.mod(j+1, nodes_y_coarse), 0])
+        fine[l, 2 * i, 2 * j, 0] = coarse[l, i, j, 0]
+        fine[l, wp.mod(2 * i + 1, nodes_x_fine), 2 * j, 0] = 0.5 * (coarse[l, i, j, 0] + coarse[l, wp.mod(i + 1, nodes_x_coarse), j, 0])
+        fine[l, i, wp.mod(2 * j + 1, nodes_y_fine), 0] = 0.5 * (coarse[l, i, j, 0] + coarse[l, i, wp.mod(j + 1, nodes_y_coarse), 0])
+        fine[l, wp.mod(2 * i + 1, nodes_x_fine), wp.mod(2 * j + 1, nodes_y_fine), 0] = 0.25 * (
+            coarse[l, i, j, 0]
+            + coarse[l, wp.mod(i + 1, nodes_x_coarse), j, 0]
+            + coarse[l, i, wp.mod(j + 1, nodes_y_coarse), 0]
+            + coarse[l, wp.mod(i + 1, nodes_x_coarse), wp.mod(j + 1, nodes_y_coarse), 0]
+        )
+
 
 @wp.kernel
 def restrict(coarse: Any, fine: Any, dim: Any):
     i, j, k = wp.tid()
     for l in range(dim):
-        coarse[l, i, j, 0] = fine[l, 2*i, 2*j, 0]
-        
-
+        coarse[l, i, j, 0] = fine[l, 2 * i, 2 * j, 0]
 
 
 class MultigridSolver:
     """
     A class implementing a multigrid iterative solver for elliptic PDEs.
-   """ 
+    """
+
     def __init__(self, nodes_x, nodes_y, length_x, length_y, dt, E, nu, force_load, gamma, timesteps, max_levels=None):
         compute_backend = ComputeBackend.WARP
         precision_policy = PrecisionPolicy.FP32FP32
@@ -107,36 +157,60 @@ class MultigridSolver:
         xlb.init(velocity_set=velocity_set, default_backend=compute_backend, default_precision_policy=precision_policy)
 
         solid_simulation = SimulationParams()
-        solid_simulation.set_parameters(E=E, nu=nu, dx=1, dt=1, L=1, T=1, kappa=1, theta=1.0 / 3.0) #just placeholder, so E and nu get past to all levels
+        solid_simulation.set_parameters(
+            E=E, nu=nu, dx=1, dt=1, L=1, T=1, kappa=1, theta=1.0 / 3.0
+        )  # just placeholder, so E and nu get past to all levels
         self.timesteps = timesteps
 
-        #TODO: boundary conditions
-
+        # TODO: boundary conditions
 
         # Determine maximum possible levels
-        self.max_possible_levels = min(int(np.log2(nodes_x-1)), int(np.log2(nodes_y-1))) + 1
-        
+        self.max_possible_levels = min(int(np.log2(nodes_x - 1)), int(np.log2(nodes_y - 1))) + 1
+
         if max_levels is None:
             self.max_levels = self.max_possible_levels
         else:
             self.max_levels = min(max_levels, self.max_possible_levels)
 
-        #setup levels
+        # setup levels
         self.levels = list()
         for i in range(self.max_levels):
-            nx_level = (nodes_x - 1) // (2 ** i) + 1 #IMPORTANT: only works with nodes as power of two at the moment
-            ny_level = (nodes_y - 1) // (2 ** i) + 1
+            nx_level = (nodes_x - 1) // (2**i) + 1  # IMPORTANT: only works with nodes as power of two at the moment
+            ny_level = (nodes_y - 1) // (2**i) + 1
             dx = length_x / float(nx_level)
             dy = length_y / float(ny_level)
-            dt_level = dt / (4**i)
+            dt_level = dt * (4**i)
             assert math.isclose(dx, dy)
-            level  = Level(nx_level, ny_level, dx, dt_level, force_load, gamma, compute_backend, velocity_set, precision_policy)
+            level = Level(
+                nodes_x=nx_level,
+                nodes_y=ny_level,
+                dx=dx,
+                dt=dt_level,
+                force_load=force_load,
+                gamma=gamma,
+                compute_backend=compute_backend,
+                velocity_set=velocity_set,
+                precision_policy=precision_policy,
+            )
             self.levels.append(level)
 
-        assert(self.max_levels == 2)
-    
+        assert self.max_levels == 2
+
+
     def work(self):
+        self.levels[1].startup()
+        macroscopics = self.levels[1].get_macroscopics()
+        for i in range(min(self.timesteps, 50)):
+            self.levels[1].perform_smoothing()
+            macroscopics = self.levels[1].get_macroscopics()
+        # now switch to fine mesh
+        wp.launch(interpolate, inputs=[self.levels[1].f_1, self.levels[0].f_1, self.levels[1].nodes_x, self.levels[1].nodes_y, 9], dim=self.levels[1].f_1.shape[1:])
         self.levels[0].startup()
+        #macroscopics = self.levels[0].get_macroscopics()
+        for i in range(max(self.timesteps-50, 0)):
+            self.levels[0].perform_smoothing()
+            macroscopics = self.levels[0].get_macroscopics()
+        """self.levels[0].startup()
         macroscopics = self.levels[0].get_macroscopics()
         for i in range(self.timesteps):
             #smoothing on fine grid
@@ -148,6 +222,7 @@ class MultigridSolver:
             wp.launch(restrict, inputs=[self.levels[1].f_4, self.levels[0].f_1,9], dim=self.levels[1].f_1.shape[1:])
             self.levels[1].set_defect_correction()
             #solve on coarse grid
+            self.levels[1].startup()
             for i in range(1):
                 self.levels[1].perform_smoothing()
             #get approximation of error on coarse grid
@@ -156,6 +231,5 @@ class MultigridSolver:
             wp.launch(interpolate, inputs=[error_approx, self.levels[0].f_4, self.levels[1].nodes_x, self.levels[1].nodes_y, 9], dim=error_approx.shape[1:])
             #add error to current approximation
             wp.launch(utils.add_populations, inputs=[self.levels[0].f_1, self.levels[0].f_4, self.levels[0].f_1, 9], dim=self.levels[0].f_1.shape[1:])
-            macroscopics = self.levels[0].get_macroscopics()
+            macroscopics = self.levels[0].get_macroscopics()"""
         return macroscopics
-            

xlb/experimental/thermo_mechanical/multigrid_runner.py
@@ -22,7 +22,7 @@ from xlb.experimental.thermo_mechanical.multigrid import MultigridSolver
 def write_results(norms_over_time, name):
     with open(name, "w", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow(["Timestep", "L2", "Linf"])
+        writer.writerow(["timestep", "l2_disp", "linf_disp", "l2_stress", "linf_stress"])
         writer.writerows(norms_over_time)
 
 
@@ -35,9 +35,14 @@ if __name__ == "__main__":
     xlb.init(velocity_set=velocity_set, default_backend=compute_backend, default_precision_policy=precision_policy)
 
     # initialize grid
-    nodes_x = 30
-    nodes_y = 30
+    nodes_x = 20
+    nodes_y = 20
     grid = grid_factory((nodes_x, nodes_y), compute_backend=compute_backend)
+    nodes_x_2 = (int)(nodes_x * 0.5)
+    nodes_y_2 = (int)(nodes_y * 0.5)
+    grid_2 = grid_factory((nodes_x_2, nodes_y_2), compute_backend=compute_backend)
+    print(type(grid))
+    print(type(grid_2))
 
     # get discretization
     length_x = 1
@@ -45,7 +50,8 @@ if __name__ == "__main__":
     dx = length_x / float(nodes_x)
     dy = length_y / float(nodes_y)
     assert math.isclose(dx, dy)
-    timesteps = 10000
+    dx_2 = length_x / float(nodes_x_2)
+    timesteps = 100
     dt = 0.001
 
     # params
@@ -64,6 +70,10 @@ if __name__ == "__main__":
         utils.get_function_on_grid(manufactured_v, x, y, dx, grid),
     ])
     force_load = utils.get_force_load((manufactured_u, manufactured_v), x, y)
+    expected_displacement_2 = np.array([
+        utils.get_function_on_grid(manufactured_u, x, y, dx_2, grid_2),
+        utils.get_function_on_grid(manufactured_v, x, y, dx_2, grid_2),
+    ])
 
     # get expected stress
     s_xx, s_yy, s_xy = utils.get_expected_stress((manufactured_u, manufactured_v), x, y)
@@ -72,21 +82,44 @@ if __name__ == "__main__":
         utils.get_function_on_grid(s_yy, x, y, dx, grid),
         utils.get_function_on_grid(s_xy, x, y, dx, grid),
     ])
+    expected_stress_2 = np.array([
+        utils.get_function_on_grid(s_xx, x, y, dx_2, grid_2),
+        utils.get_function_on_grid(s_yy, x, y, dx_2, grid_2),
+        utils.get_function_on_grid(s_xy, x, y, dx_2, grid_2),
+    ])
 
     potential, boundary_array, boundary_values = None, None, None
 
-
     # adjust expected solution
     expected_macroscopics = np.concatenate((expected_displacement, expected_stress), axis=0)
+    expected_macroscopics_2 = np.concatenate((expected_displacement_2, expected_stress_2), axis=0)
     expected_macroscopics = utils.restrict_solution_to_domain(expected_macroscopics, potential, dx)
+    expected_macroscopics = utils.restrict_solution_to_domain(expected_macroscopics, potential, dx_2)
+    norms_over_time = list()
 
-    for i in range(1000):
-        multigrid_solver = MultigridSolver(nodes_x=nodes_x, nodes_y=nodes_y, length_x=length_x, length_y=length_y, dt=dt, E=E, nu=nu, force_load=force_load, gamma=0.8, timesteps=i, max_levels=2) 
+    for i in range(timesteps):
+        multigrid_solver = MultigridSolver(
+            nodes_x=nodes_x,
+            nodes_y=nodes_y,
+            length_x=length_x,
+            length_y=length_y,
+            dt=dt,
+            E=E,
+            nu=nu,
+            force_load=force_load,
+            gamma=0.8,
+            timesteps=i,
+            max_levels=2,
+        )
         macroscopics = multigrid_solver.work()
-        norms_over_time = list()
-        l2_disp, linf_disp, l2_stress, linf_stress = utils.process_error(macroscopics, expected_macroscopics, 0, dx, norms_over_time)
+        try:
+            l2_disp, linf_disp, l2_stress, linf_stress = utils.process_error(macroscopics, expected_macroscopics, i, dx, norms_over_time)
+        except:
+            l2_disp, linf_disp, l2_stress, linf_stress = utils.process_error(macroscopics, expected_macroscopics_2, i, dx*2, norms_over_time)
+
         # write out error norms
         print(l2_disp, linf_disp, l2_stress, linf_stress)
         utils.output_image(macroscopics, i, "figure", None, None)
-
     
+    write_results(norms_over_time, "results.csv")
+    print("done")

xlb/experimental/thermo_mechanical/solid_stepper.py
@@ -43,8 +43,8 @@ class SolidsStepper(Stepper):
         dx = params.dx
         T = params.T
         L = params.L
+        kappa = params.kappa
         dt = params.dt
-
 
         # ----------calculate omega------------
         omega_11 = 1.0 / (mu / theta + 0.5)
@@ -61,8 +61,8 @@ class SolidsStepper(Stepper):
         # ----------handle force load---------
         b_x_scaled = lambda x_node, y_node: force_load[0](
             x_node * dx + 0.5 * dx, y_node * dx + 0.5 * dx
-        )  # force now dimensionless, and can get called with the indices of the grid nodes
-        b_y_scaled = lambda x_node, y_node: force_load[1](x_node * dx + 0.5 * dx, y_node * dx + 0.5 * dx)
+        )*dt  # force now dimensionless, and can get called with the indices of the grid nodes
+        b_y_scaled = lambda x_node, y_node: force_load[1](x_node * dx + 0.5 * dx, y_node * dx + 0.5 * dx)*dt
         host_force_x = np.fromfunction(
             b_x_scaled, shape=(self.grid.shape[0], self.grid.shape[1])
         )  # create array with force evaluated at the grid points

xlb/experimental/thermo_mechanical/solid_utils.py
@@ -104,23 +104,26 @@ def copy_populations(origin: wp.array4d(dtype=Any), dest: wp.array4d(dtype=Any),
     for l in range(dim):
         dest[l, i, j, 0] = origin[l, i, j, 0]
 
+
 @wp.kernel
 def get_residual(f_previous: Any, f_now: Any, residual: Any, dim: Any):
     i, j, k = wp.tid()
     for l in range(dim):
-        residual[l, i, j, 0] = f_now[l,i,j,0]-f_previous[l,i,j,0]
+        residual[l, i, j, 0] = f_now[l, i, j, 0] - f_previous[l, i, j, 0]
+
 
 @wp.kernel
 def subtract_populations(a: Any, b: Any, c: Any, dim: Any):
     i, j, k = wp.tid()
     for l in range(dim):
-        c[l, i, j, 0] = b[l,i,j,0]-a[l,i,j,0]
+        c[l, i, j, 0] = b[l, i, j, 0] - a[l, i, j, 0]
+
 
 @wp.kernel
 def add_populations(a: Any, b: Any, c: Any, dim: Any):
-    i,j,k = wp.tid()
+    i, j, k = wp.tid()
     for l in range(dim):
-        c[l,i,j,0] = a[l,i,j,0] + b[l,i,j,0]
+        c[l, i, j, 0] = a[l, i, j, 0] + b[l, i, j, 0]
 
 
 @wp.func
@@ -163,19 +166,15 @@ def calc_equilibrium(m: solid_vec, theta: Any):
 
 def get_force_load(manufactured_displacement, x, y):
     params = SimulationParams()
-    mu = params.mu
-    K = params.K
-    L = params.L
-    T = params.T
-    dt = params.T
-    kappa = params.kappa
+    mu = params.mu_unscaled
+    K = params.K_unscaled
     man_u = manufactured_displacement[0]
     man_v = manufactured_displacement[1]
     b_x = -mu * (sympy.diff(man_u, x, x) + sympy.diff(man_u, y, y)) - K * sympy.diff(sympy.diff(man_u, x) + sympy.diff(man_v, y), x)
     b_y = -mu * (sympy.diff(man_v, x, x) + sympy.diff(man_v, y, y)) - K * sympy.diff(sympy.diff(man_u, x) + sympy.diff(man_v, y), y)
-    bx_scaled = b_x * L * L * kappa * (dt / T)
-    by_scaled = b_y * L * L * kappa * (dt / T)
-    return (np.vectorize(sympy.lambdify([x, y], bx_scaled, "numpy")), np.vectorize(sympy.lambdify([x, y], by_scaled, "numpy")))
+    #bx_scaled = b_x * L * L * kappa * (dt / T)
+    #by_scaled = b_y * L * L * kappa * (dt / T)
+    return (np.vectorize(sympy.lambdify([x, y], b_x, "numpy")), np.vectorize(sympy.lambdify([x, y], b_y, "numpy")))
 
 
 def get_function_on_grid(f, x, y, dx, grid):